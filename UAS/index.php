48204279a54a177945e241279c6b075cc5556964
jkev31 <kevintsan00@gmail.com>
2026-07-02 11:14:32 +0700

    add bs5, jquery, dtbl, chrt

--- UAS/index.php
+++ UAS/index.php
@@ -5,13 +5,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js@4.5.0"></script>
   <link href="https://cdn.datatables.net/2.3.8/css/dataTables.dataTables.min.css" rel="stylesheet">
+
+  <script src="https://cdn.datatables.net/2.3.8/js/dataTables.min.js"></script> 
+  <!-- <link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/chart.js"></script>
+  <link href="DataTables/datatables.min.css" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
-  <script src="https://cdn.datatables.net/2.3.8/js/dataTables.min.js"></script>
+  <script src="DataTables/dataTables.min.js"></script> -->
 </head>
 
 <body>
@@ -26,19 +33,19 @@
             <div class="collapse navbar-collapse" id="mynavbar">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <button class="nav-link" id="dashboard">dashboard</button>
+                        <a href="#" class="nav-link" id="dashboard">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <button class="nav-link" id="nav-produk">produk</button>
+                        <a href="#" class="nav-link" id="nav-produk">produk</a>
                     </li>
                     <li class="nav-item">
-                        <button class="nav-link" id="nav-supplier">supplier</button>
+                        <a href="#" class="nav-link" id="nav-supplier">supplier</a>
                     </li>
                     <li class="nav-item">
-                        <button class="nav-link" id="pembelian">pembelian</button>
+                        <a href="#" class="nav-link" id="pembelian">pembelian</a>
                     </li>
                     <li class="nav-item">
-                        <button class="nav-link" id="penjualan">penjualan</button>
+                        <a href="#" class="nav-link" id="penjualan">penjualan</a>
                     </li>
                 </ul>
       
